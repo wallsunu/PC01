73c07579cb64d537a80d96cc6f6ae1e7e9cff33c
wallsunu <vivifloresmontes25@gmail.com>
2026-03-28 11:20:27 -0600

    comentario: agregar detalle de campaña

--- Controllers/CampanasControllers.cs
+++ Controllers/CampanasControllers.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using PortalCampanas.Services;
+using System.Linq;
 
 namespace PortalCampanas.Controllers
 {
@@ -10,6 +11,14 @@ namespace PortalCampanas.Controllers
             var campanas = CampanaService.ObtenerCampanas();
             return View(campanas);
         }
+        public IActionResult Detalle(int id)
+        {
+            var campana = CampanaService.ObtenerCampanas()
+                .FirstOrDefault(c => c.Id == id);
+
+            return View(campana);
+        }
     }
+    
 }
 
